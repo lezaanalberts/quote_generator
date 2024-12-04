1788a42335ad875ba557a4890ec357812f1b5007
lezaanalberts <lezaanalbertscoding@gmail.com>
2024-12-04 12:21:28 +0200

    final product

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+function App(props) {
+  const [quote,setQuote] = useState(undefined);
+
+  const getQuote= async()=>{
+    const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
+    const options = {
+	    method: 'GET',
+	    headers: {
+      'x-rapidapi-key': 'db0fb0cfcamsh65525489803fe56p1ed472jsn7ca1dcefee7a',
+      'x-rapidapi-host': 'quotes15.p.rapidapi.com'
+	    }
+    };
+    try{
+      const response= await fetch(url,options);
+      setQuote(await response.json());
+    } catch(error){
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {!quote && (<button className="button" onClick={()=>
+        getQuote ()}>Get Quote!
+        </button>)}
+
+      {quote && (
+        <div className="quote">
+          <h1>{quote.content}</h1>
+          <h2>{quote.originator.name}</h2>
+        </div>
+      )}
     </div>
   );
 }
